f68aefbf1ff4e34b522be8af9b408d05554ae9bd
Jordan Papaleo <papaleowebdev@gmail.com>
2020-02-21 12:54:42 -0800

    Formatting

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  globals: { "ts-jest": { tsConfig: "tsconfig.json" } },
-  moduleFileExtensions: [ "ts", "js" ],
+  globals: { 'ts-jest': { tsConfig: 'tsconfig.json' } },
+  moduleFileExtensions: ['ts', 'js'],
   roots: ['<rootDir>/src'],
-  testEnvironment: "node",
-  testMatch: [ '**/*.test.ts' ],
-  transform: { "^.+\\.(ts|tsx)$": "ts-jest" }
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  transform: { '^.+\\.(ts|tsx)$': 'ts-jest' }
 }
